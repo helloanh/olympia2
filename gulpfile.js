0ce032c974e2cf5ff0b674fa89d00ceffd1740e1
Anh K. Hoang <kim_anh88@berkeley.edu>
2017-01-13 11:37:44 -0500

    rename theme, edit gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -11,38 +11,38 @@ var pngquant = require('imagemin-pngquant');
 
 
 gulp.task('imagemin', function () {
-    return gulp.src('./wp-content/themes/olympos/images/*')
+    return gulp.src('./wp-content/themes/olympia2/images/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
-        .pipe(gulp.dest('./wp-content/themes/olympos/images'));
+        .pipe(gulp.dest('./wp-content/themes/olympia2/images'));
 });
 
 
 gulp.task('sass', function () {
-  gulp.src('./wp-content/themes/olympos/sass/**/*.scss')
+  gulp.src('./wp-content/themes/olympia2/sass/**/*.scss')
     .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./wp-content/themes/olympos'));
+    .pipe(gulp.dest('./wp-content/themes/olympia2'));
 });
 
 
 gulp.task('uglify', function() {
-  gulp.src('./wp-content/themes/olympos/lib/*.js')
-    .pipe(uglify('olympos.min.js'))
-    .pipe(gulp.dest('./wp-content/themes/olympos/js'))
+  gulp.src('./wp-content/themes/olympia2/lib/*.js')
+    .pipe(uglify('olympia2.min.js'))
+    .pipe(gulp.dest('./wp-content/themes/olympia2/js'))
 });
 
 gulp.task('watch', function(){
     livereload.listen();
 
-    gulp.watch('./wp-content/themes/olympos/sass/**/*.scss', ['sass']);
-    gulp.watch('./wp-content/themes/olympos/lib/*.js', ['uglify']);
-    gulp.watch(['./wp-content/themes/olympos/style.css', './wp-content/themes/olympos/*.php', './wp-content/themes/olympos/js/*.js', './wp-content/themes/olympos/parts/**/*.php'], function (files){
+    gulp.watch('./wp-content/themes/olympia2/sass/**/*.scss', ['sass']);
+    gulp.watch('./wp-content/themes/olympia2/lib/*.js', ['uglify']);
+    gulp.watch(['./wp-content/themes/olympia2/style.css', './wp-content/themes/olympia2/*.php', './wp-content/themes/olympia2/js/*.js', './wp-content/themes/olympia2/parts/**/*.php'], function (files){
         livereload.changed(files)
     });
 });
